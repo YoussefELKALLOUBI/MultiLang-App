97d7814bd68ed37485e1d63a493cd6ecc833d6cb
Youssef EL-KALLOUBI <youssefelkalloubi54@gmail.com>
2024-06-18 22:11:11 +0100

    separate a conf var

--- multilang_site/multilang_site/settings.py
+++ multilang_site/multilang_site/settings.py
@@ -136,3 +136,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+openai_api_key = "sk-proj-Hr3kioMTiuVIr2CMFCgnT3BlbkFJcaSWsFlVHGshhHnraN2X"
